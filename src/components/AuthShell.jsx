1871e01252f6547eb4791d27555687e688047b43
Shervin Dale M. Tabernero <shervindalemaupotabern@gmail.com>
2026-07-02 22:23:54 +0800

    style(AuthShell): add animated living gradient blobs to auth screens

--- src/components/AuthShell.jsx
+++ src/components/AuthShell.jsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 export default function AuthShell({ title, children, footer }) {
   return (
     <div className="relative flex min-h-screen flex-col text-white">
-      {/* Ambient gradient blobs */}
+      {/* Living ambient gradient blobs */}
       <div className="pointer-events-none absolute inset-0 overflow-hidden">
-        <div className="absolute -left-24 -top-24 h-72 w-72 rounded-full bg-accent/30 blur-3xl" />
-        <div className="absolute -bottom-24 right-0 h-72 w-72 rounded-full bg-accent-dark/30 blur-3xl" />
+        <div className="animate-blob-a absolute -left-24 -top-24 h-72 w-72 rounded-full bg-accent/40 blur-3xl" />
+        <div className="animate-blob-b absolute -bottom-24 right-0 h-72 w-72 rounded-full bg-accent-dark/40 blur-3xl" />
+        <div className="animate-blob-c absolute left-1/3 top-1/2 h-64 w-64 rounded-full bg-accent/25 blur-3xl" />
       </div>
 
       <header className="relative p-6">
